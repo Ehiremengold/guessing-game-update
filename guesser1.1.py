53eb4f3c0cd8c258b3e4c38853a791b5a6234f3b
Ibhafidon ehiremen francis <Ehiremengold@gmail.com>
2019-04-18 14:37:18 -0700

    Update guesser1.1.py

--- guesser1.1.py
+++ guesser1.1.py
@@ -1,4 +1,5 @@
 
+#Declaring variables
 name = input('Enter your name:')
 guess = int()
 guessnumber = 2
@@ -9,20 +10,21 @@ out_of_guess = False
 welcome = print('Well, hello ' + name + ', I\'m q-bot')
 
 question = input('I\'m thinking of a number. Do you want to guess what it is(y/n)?')
+#questionnaire
 if question == 'n':
     print('oh..okay')
 elif question == 'y':
     print('Great!')
 else:
-    print('invalid input')
-
+    print('Invalid input, i guess you want to play!')
+### As long as user's input is wrong the question is to continue
 while guessnumber != guess and not out_of_guess:
      if tries < max_tries:
          guess = int(input('Guess the number:'))
          if guess > guessnumber:
-             print('Guess a lower number')
+             print('You are a little bit way off, guess a lower number')
          elif guess < guessnumber:
-             print('Guess a higher number')
+             print('You are a little bit way off, guess a higher number')
          tries += 1
      else:
          out_of_guess = True
